60d2682b7723a08953b3936e3f34c1a2c1fac401
Roland Haas <rhaas@ncsa.illinois.edu>
2021-02-21 17:12:59 -0600

    gather: tighten Python code

--- examples/gather.py
+++ examples/gather.py
@@ -3,25 +3,26 @@
 import mpi4py.MPI as MPI
 import numpy
 
-rank = MPI.COMM_WORLD.Get_rank()
-sz = MPI.COMM_WORLD.Get_size()
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+sz = comm.Get_size()
 
-myvals = numpy.empty(shape=2, dtype='f')
+myvals = numpy.empty(2)
 
 # sest up an array of values 0...2*sz
 if(rank == 0):
-  values = numpy.arange(0,2*sz,dtype='f') #.reshape((sz,2))
-  MPI.COMM_WORLD.Scatter(values, myvals, root=0)
+  values = numpy.arange(2.*sz)
+  comm.Scatter(values, myvals, root=0)
 else:
-  MPI.COMM_WORLD.Scatter(None, myvals, root=0)
+  comm.Scatter(None, myvals, root=0)
 
 # have each rank square its data
 myvals = myvals**2
 
 # gather results back to root rank
 if(rank == 0):
-  values = numpy.empty(shape=2*sz, dtype='f')
-  MPI.COMM_WORLD.Gather(myvals, values, root=0)
+  values = numpy.empty(2*sz)
+  comm.Gather(myvals, values, root=0)
   print("Got squares: "+str(values))
 else:
-  MPI.COMM_WORLD.Gather(myvals, None, root=0)
+  comm.Gather(myvals, None, root=0)

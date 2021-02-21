9cb6de9334bc92eb44e871aff00875106951e7f8
Roland Haas <rhaas@ncsa.illinois.edu>
2021-02-21 15:40:20 -0600

    worker: restructure code to be more legible and extendable

--- examples/worker.py
+++ examples/worker.py
@@ -3,31 +3,37 @@
 import mpi4py.MPI as MPI
 import numpy as np
 
-dummy_tag = 42
-
 rank = MPI.COMM_WORLD.Get_rank()
 sz = MPI.COMM_WORLD.Get_size()
 
-if (rank == 0):
-  numbertosend = np.array([4.0])
+# fix seed so that the same numbers are used each time
+np.random.seed(17)
+
+def manager():
+  numberstosend = np.random.rand(sz-1)
   for i in range(1,sz):
-    MPI.COMM_WORLD.Send(numbertosend, i, dummy_tag)
-else:
-  numbertoreceive = np.empty(shape=(1),dtype=float)
-  MPI.COMM_WORLD.Recv(numbertoreceive, 0, MPI.ANY_TAG)
-  result = numbertoreceive * rank
+    MPI.COMM_WORLD.Send(numberstosend[i-1], i)
 
-for i in range(1,sz):
-  if (i == rank):
-    print ("PE %d's result is %f" % (rank, result[0]))
-  MPI.COMM_WORLD.Barrier()
-
-if (rank == 0):
   result = np.zeros(shape=(1), dtype=float)
   for i in range(1,sz):
     numbertoreceive = np.empty(shape=(1),dtype=float)
-    MPI.COMM_WORLD.Recv(numbertoreceive, MPI.ANY_SOURCE, MPI.ANY_TAG)
+    status = MPI.Status()
+    MPI.COMM_WORLD.Recv(numbertoreceive, status=status)
+    print ("Received %f from PE %d" % (numbertoreceive[0], status.Get_source()))
     result = result + numbertoreceive
   print ("Total is %f" % result[0])
+
+def worker():
+  numbertoreceive = np.empty(shape=(1),dtype=float)
+  MPI.COMM_WORLD.Recv(numbertoreceive, 0)
+
+  result = numbertoreceive * rank
+
+  print ("PE %d received %f and computed %f" % (rank, numbertoreceive[0], result[0]))
+
+  MPI.COMM_WORLD.Send(result, 0)
+
+if (rank == 0):
+  manager()
 else:
-  MPI.COMM_WORLD.Send(result, 0, dummy_tag)
+  worker()
